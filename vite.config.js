40cb129378842cdfbecd8b6107c49e1dd40615ed
Prokhar Kulak <prokhorkulak@gmail.com>
2026-03-20 21:06:14 +0300

    upd vite config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require("vite");
 
 module.exports = defineConfig({
   base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        // Один предсказуемый URL: старые деплои / кэш ищут main.js; хеш только у чанков.
+        entryFileNames: "main.js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
+  },
 });
